91abf77c3eb9e2d8ccee4e18958a5860991c29f6
Murad Cafarov <cafarovmurad9@gmail.com>
2019-11-23 01:13:17 +0300

    FullProject

--- HomeTask.LibraryProject/Entity/Author.cs
+++ HomeTask.LibraryProject/Entity/Author.cs
@@ -10,7 +10,6 @@ namespace HomeTask.LibraryProject.Entity
         public int Id { get; set; }
         public string AuthorName { get; set; }
         public List<Book> Books { get; set; }
-        public int? Authorage { get; set; }
 
     }
 }
